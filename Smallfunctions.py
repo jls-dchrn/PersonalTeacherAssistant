24b25bdccdd893c482cd6646eee8f4774c5ebe50
NikolajMeineche <n_meineche@hotmail.dk>
2024-06-21 14:43:50 +0900

    template for work

---
+++ Smallfunctions.py
@@ -0,0 +1,73 @@
+
+
+class gpt():
+
+    def __init__(self):
+        pass
+    """ 
+    model 
+
+    SessionMemory
+
+    initial prompt
+
+    summarization prompt
+
+    context file
+
+    """
+
+    # Ask user for username, and return context file. Add new contextfile if username wasnt found
+    # Hayato
+    # Takumi
+    def login():
+        pass
+    
+
+    # create context file for new user with an existing template
+    # the template could contain, name, level, previous summarizations
+
+    # Hayato
+    # Takumi
+    def createFile():
+        pass
+
+    # basic call to gpt api
+    #Nikolaj
+    def _sendMessage():
+        pass
+
+    
+    # use sendMessage to summarize session with summarize prompt, and save it into the context file
+    #Nikolaj
+    def summarize():
+        pass
+
+    # use sendMessage and SessionMemory for the user to ask questions
+    """ 
+    Login
+    Loop:
+        User asks question
+        Model responds
+        Model asks if user wants to continue / (rating system for model)
+    Summarize session
+    """
+    # Jules
+    def discuss():
+        pass
+
+    # resize image and file format to standard format
+    #Jules
+    def imagePrep():
+        pass
+
+
+
+
+
+
+
+
+
+
+
